50dd50563af7be5d526469e56c094529393141db
“Muzyria” <fila090580@gmail.com>
2022-07-21 17:44:50 +0300

    added TestToolTips added test_tool_tips

--- tests/widgests_test.py
+++ tests/widgests_test.py
@@ -1,4 +1,5 @@
-from pages.widgets_page import AccordianPage, AutoCompletePage, DatePickerPage, SliderPage, ProgressBarPage, TabsPage
+from pages.widgets_page import AccordianPage, AutoCompletePage, DatePickerPage, SliderPage, ProgressBarPage, TabsPage, \
+    ToolTipsPage
 
 
 class TestWidgets:
@@ -80,3 +81,14 @@ class TestWidgets:
                                                                       'is missing '
             assert use_button == 'Use' and use_content != 0, 'the tab "use" was not pressed or the text is missing'
             assert more_button == 'More' and what_content != 0, 'the tab "more" was not pressed or the text is missing'
+
+    class TestToolTips:
+        def test_tool_tips(self, driver):
+            tool_tips_page = ToolTipsPage(driver, 'https://demoqa.com/tool-tips')
+            tool_tips_page.open()
+            button_text, field_text, contrary_text, section_text = tool_tips_page.check_tool_tips()
+            # print(button_text, field_text, contrary_text, section_text, sep="\n")
+            assert button_text == 'You hovered over the Button', 'hover missing or incorrect content'
+            assert field_text == 'You hovered over the text field', 'hover missing or incorrect content'
+            assert contrary_text == 'You hovered over the Contrary', 'hover missing or incorrect content'
+            assert section_text == 'You hovered over the 1.10.32', 'hover missing or incorrect content'
